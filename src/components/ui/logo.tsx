db131c4e2950b9710dbb26f65461a8926d45c781
Daniil <127053306+Lyguh@users.noreply.github.com>
2026-01-31 19:50:20 +0300

    final make prototype dinamic pages

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Logo = () => {
+    const logoImg = '/' + "static/Logo.svg";
     return (
         <Link className={"flex items-center"} href="/">
-            <Image className={"py-[5px]"} width={70} height={70} loading={"lazy"} src={"static/Logo.svg"} alt="Logotype"/>
+            <Image className={"py-[5px]"} width={70} height={70} loading={"lazy"} src={logoImg} alt="Logotype"/>
             <p className="text-[18px] font-semibold text-[#000000] xs:hidden md:flex pl-[15px]">
                 ВИРТУАЛЬНЫЙ МУЗЕЙ
                 <span className="pl-[15px]">
