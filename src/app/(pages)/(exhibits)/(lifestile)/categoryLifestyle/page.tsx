6b0663602ed5a1f46f4cfd3f5f1bad755c297b5f
Taggconr <deprofie082@gmail.com>
2026-02-18 09:55:19 +0300

    make full machanic web-site

--- src/app/(pages)/(exhibits)/(lifestile)/categoryLifestyle/page.tsx
+++ src/app/(pages)/(exhibits)/(lifestile)/categoryLifestyle/page.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { ALL_EXHIBITS_DATA } from "@/data/allExhibits-data";
 import Link from "next/link";
+import Banner from "@/components/ui/banner";
+import Container from "@/components/shared/container";
+import Image from "next/image";
 
 const CategoryLifestilePage = () => {
     const CURRENT_CATEGORY_ID = 'lifestyle';
@@ -10,22 +13,21 @@ const CategoryLifestilePage = () => {
     );
 
     return (
-        <div>
-            <h1 className="text-2xl font-bold mb-6">
-                Товары категории лайфстайл
-            </h1>
+        <Container className={"mt-[40px]"}>
+            <Banner className={"text-2xl font-bold mb-6"} text={"БЫТ"} />
             {filteredExhibits.length > 0 ? (
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="mt-[120px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {filteredExhibits.map(exhibit => (
-                        <Link  href={`/exhibit/${exhibit.id}`} key={exhibit.id} className="border rounded-lg p-4 shadow-sm hover:shadow-md transition-shadow">
-                            <h3 className="text-lg font-semibold">{exhibit.title}</h3>
+                        <Link  href={`/exhibit/${exhibit.id}`} key={exhibit.id} className="border-[1px] border-[#BD9E7B] bg-[#F5E5D3] flex flex-col text-center rounded-lg p-4">
+                            <Image height={100} width={100} className={"w-full h-[425px] object-cover"} src={exhibit.images[1]} alt={exhibit.title} />
+                            <h3 className="text-lg font-semibold mt-[26px]">{exhibit.title}</h3>
                         </Link>
                     ))}
                 </div>
             ) : (
                 <p className="text-gray-600 italic">В этой категории пока нет товаров</p>
             )}
-        </div>
+        </Container>
     );
 };
 
--- src/app/(pages)/(exhibits)/(vov)/categoryVov/page.tsx
+++ src/app/(pages)/(exhibits)/(vov)/categoryVov/page.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { ALL_EXHIBITS_DATA } from "@/data/allExhibits-data";
+import Link from "next/link";
+import Banner from "@/components/ui/banner";
+import Container from "@/components/shared/container";
+import Image from "next/image";
 
 const CategoryVovPage = () => {
+    const CURRENT_CATEGORY_ID = 'vov';
+
+    const filteredExhibits = ALL_EXHIBITS_DATA.filter(exhibit =>
+        exhibit.categoryId === CURRENT_CATEGORY_ID
+    );
+
     return (
-        <div className="bg-green-900">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque fuga minima modi nostrum, numquam ratione recusandae repellendus sit veniam veritatis.
-        </div>
+        <Container className={"mt-[40px]"}>
+            <Banner className={"text-2xl font-bold mb-6"} text={"БЫТ"} />
+            {filteredExhibits.length > 0 ? (
+                <div className="mt-[120px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {filteredExhibits.map(exhibit => (
+                        <Link  href={`/exhibit/${exhibit.id}`} key={exhibit.id} className="border-[1px] border-[#BD9E7B] bg-[#F5E5D3] flex flex-col text-center rounded-lg p-4">
+                            <Image height={100} width={100} className={"w-full h-[425px] object-cover"} src={exhibit.images[1]} alt={exhibit.title} />
+                            <h3 className="text-lg font-semibold mt-[26px]">{exhibit.title}</h3>
+                        </Link>
+                    ))}
+                </div>
+            ) : (
+                <p className="text-gray-600 italic">В этой категории пока нет товаров</p>
+            )}
+        </Container>
     );
 };
 
