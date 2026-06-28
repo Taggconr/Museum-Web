de17e09ff7980b1de1024ce18bcbbb9bade441d8
Taggconr <deprofie082@gmail.com>
2026-06-29 01:21:18 +0300

    go back

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
   return (
     <div className="">
       <main>
-          <Container>
-              <HeroBaner />
-              <GoExhibits />
-              <MissionsCards />
-          </Container>
+        <Container>
+          <HeroBaner />
+          <GoExhibits />
+          <MissionsCards />
+        </Container>
 
       </main>
     </div>
